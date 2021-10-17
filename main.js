19e78de7ac16eba52f6e21e4b77320d9126dc8a8
caleb-bender <caleb.bender@misterwaverlyinc.com>
2021-10-17 14:40:32 -0700

    completed assignment

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const serveFile = (pathToFile, res, type="text/html") => {
 
 const serve404NotFound = (res) => {
     res.writeHead(404, { "Content-Type": "text/html" });
-    fs.readFile(`${__dirname}/public/views/404notfound.html`, (err, data) => {
+    fs.readFile(`${__dirname}/public/html/404notfound.html`, (err, data) => {
         if (err) {
             res.writeHead(500, { "Content-Type": "text/html" });
             res.write("An internal server error occurred.");
@@ -32,12 +32,14 @@ const serve404NotFound = (res) => {
 
 
 http.createServer(function (req, res) {
-    if (req.url === "/cars") {
-        serveFile("/public/views/cars.html", res);
+    if (req.url === "/") {
+        serveFile("/public/html/index.html", res);
+    } else if (req.url === "/cars") {
+        serveFile("/public/html/cars.html", res);
     } else if (req.url === "/dogs") {
-        serveFile("/public/views/dogs.html", res);
+        serveFile("/public/html/dogs.html", res);
     } else if (req.url === "/cars/new") {
-        serveFile("/public/views/newcar.html", res);
+        serveFile("/public/html/newcar.html", res);
     } else if (req.url.startsWith("/public/styles/")) {
         serveFile(req.url, res, "text/css");
     } else if (req.url.startsWith("/public/images/")) {
